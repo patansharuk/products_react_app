11d368e98fbefd9bc955a267efc7af903d6613aa
sharukhan <sharukhan@webkorps.com>
2024-05-24 19:21:54 +0530

    added request interceptor for token

--- src/components/Swagger/swagger.js
+++ src/components/Swagger/swagger.js
@@ -1,8 +1,21 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css"
 
+const getAccessToken = () => {
+    return localStorage.getItem('jwt_token')
+}
+
+const requestInterceptor = (req) => (
+    {
+        ...req,
+        headers: {
+            Authorization: `Bearer ${getAccessToken()}`
+        },
+    }
+)
+
 const Swagger = () => {
-    return <SwaggerUI url="http://localhost:3002/api-docs/v1/swagger.json" />
+    return <SwaggerUI url="http://localhost:3002/api-docs/v1/swagger.json" requestInterceptor={requestInterceptor}/>
 }
 
 export default Swagger
