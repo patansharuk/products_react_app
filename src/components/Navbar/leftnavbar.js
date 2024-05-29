1cbc7c9bde624771547cf9b34e15e2aa5df7f185
sharukhan <sharukhan@webkorps.com>
2024-05-29 21:02:50 +0530

    fixed  button issues

--- src/components/Navbar/leftnavbar.js
+++ src/components/Navbar/leftnavbar.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Menu from './menu';
+import { useState } from "react";
+import { Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
-const Leftnavbar = ({user_details, make_logout}) => {
+const Leftnavbar = ({ user_details, make_logout }) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -15,16 +15,26 @@ const Leftnavbar = ({user_details, make_logout}) => {
         Menu
       </Button>
 
-      <Offcanvas show={show} onHide={handleClose} className='d-md-none' backdropClassName='d-md-none'>
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        className="d-md-none"
+        backdropClassName="d-md-none"
+      >
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Products React</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-            <Menu user_details={user_details} make_logout={make_logout}/>
+          Signed in as:{" "}
+          <span className="badge bg-info">{user_details.email}</span>
+          <hr className="d-md-none" />
+          <Button variant="danger" className="ms-2" onClick={make_logout}>
+            Logout
+          </Button>
         </Offcanvas.Body>
       </Offcanvas>
     </>
   );
-}
+};
 
 export default Leftnavbar;
