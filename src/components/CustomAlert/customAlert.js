dc7f3c23248e23e3cbeb8cdc8c76e70a20a732a8
sharukhan <sharukhan@webkorps.com>
2024-05-31 02:45:53 +0530

    changesin  alert comp

--- src/components/CustomAlert/customAlert.js
+++ src/components/CustomAlert/customAlert.js
@@ -1,22 +1,16 @@
-import { useEffect } from 'react';
 import { useState } from 'react';
-import Alert from 'react-bootstrap/Alert';
-import Button from 'react-bootstrap/Button';
+import { Fade, Alert } from 'react-bootstrap';
 
 function AlertDismissible({children}) {
   const [show, setShow] = useState(true);
 
-  useEffect(()=>{
-    setTimeout(()=>{
-        setShow(false)
-    }, 5000)
-  },[])
-
   if (show && (children !== '') && (children !== null)) {
     return (
-      <Alert variant="danger" className='mt-2' onClose={() => setShow(false)} dismissible>
-        <Alert.Heading className='m-0'>{children}</Alert.Heading>
-      </Alert>
+      <Fade in={show} onExited={()=> setShow(false)}>
+        <Alert variant="danger" className='mt-2' onClose={() => setShow(false)} dismissible>
+          <Alert.Heading className='m-0'>{children}</Alert.Heading>
+        </Alert>
+      </Fade>
     );
   }
   return <span></span>
