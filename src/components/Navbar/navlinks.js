4b016742958cd187f959cf0f211bf699573720bb
sharukhan <sharukhan@webkorps.com>
2024-06-01 01:00:48 +0530

    added navigations

--- src/components/Navbar/navlinks.js
+++ src/components/Navbar/navlinks.js
@@ -4,16 +4,16 @@ import { Nav } from "react-bootstrap";
 const NavLinks = () => (
   <Nav>
     <Nav.Item>
-      <Nav.Link href="/">Add Product</Nav.Link>
+      <Nav.Link href="/">Home</Nav.Link>
     </Nav.Item>
     <Nav.Item>
-      <Nav.Link href="/">Dealers</Nav.Link>
+      <Nav.Link href="/dealer_details">Dealers</Nav.Link>
     </Nav.Item>
     <Nav.Item>
-      <Nav.Link href="/">Products</Nav.Link>
+      <Nav.Link href="/products">Products</Nav.Link>
     </Nav.Item>
     <Nav.Item>
-      <Nav.Link href="/">Disabled</Nav.Link>
+      <Nav.Link href="/cart">Cart</Nav.Link>
     </Nav.Item>
   </Nav>
 );
