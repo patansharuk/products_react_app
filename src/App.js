90dd3e59668b2a04480b40461df3ccc37e355724
sharukhan <sharukhan@webkorps.com>
2024-06-04 18:31:19 +0530

    added private routes and added create product component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import DealerDetails from "./components/Dealer/dealerDetails";
 import EditDealerDetail from "./components/Dealer/editDealerDetail";
 import ShowDealerDetail from "./components/Dealer/showDealerDetail";
 import Cart from "./components/Cart/cart";
+import { get_auth_token } from "./utils/authUtils";
+import CreateProduct from "./components/Products/createProduct";
+
+const PrivateRoute = ({ element }) => {
+  const token = get_auth_token();
+  if (token === null) {
+    return <Navigate to="/login" replace />;
+  }
+  return element;
+};
 
 const App = () => {
   const renderAuthenticationRoutes = () => (
@@ -20,28 +30,47 @@ const App = () => {
 
   const renderProductRoutes = () => (
     <>
-      <Route path="/products" element={<Products />} />
-      {/* <Route path="/product/create" element={<CreateProduct/>}/>
-        <Route path="/product/:id/edit" element={<EditProduct/>}/>
+      <Route
+        path="/products"
+        element={<PrivateRoute element={<Products />} />}
+      />
+      <Route
+        path="/product/create"
+        element={<PrivateRoute element={<CreateProduct />} />}
+      />
+      {/* <Route path="/product/:id/edit" element={<EditProduct/>}/>
         <Route path="/product/:id" element={<ViewProduct/>}/> */}
     </>
   );
 
   const renderDealerRoutes = () => (
     <>
-      <Route path="/dealer_details" element={<DealerDetails />} />
-      <Route path="/dealer_detail/:id/edit" element={<EditDealerDetail />} />
-      <Route path="/dealer_detail/:id" element={<ShowDealerDetail />} />
+      <Route
+        path="/dealer_details"
+        element={<PrivateRoute element={<DealerDetails />} />}
+      />
+      <Route
+        path="/dealer_detail/:id/edit"
+        element={<PrivateRoute element={<EditDealerDetail />} />}
+      />
+      <Route
+        path="/dealer_detail/:id"
+        element={<PrivateRoute element={<ShowDealerDetail />} />}
+      />
     </>
   );
 
-  const renderRootRoute = () => <Route path="/" element={<Home />} />;
-
-  const renderSwaggerRoute = () => (
-    <Route path="/swagger" element={<Swagger />} />
+  const renderRootRoute = () => (
+    <Route path="/" element={<PrivateRoute element={<Home />} />} />
   );
 
-  const renderCartRoute = () => <Route path="/cart" element={<Cart />} />;
+  const renderSwaggerRoute = () => (
+    <Route path="/swagger" element={<PrivateRoute element={<Swagger />} />} />
+  );
+
+  const renderCartRoute = () => (
+    <Route path="/cart" element={<PrivateRoute element={<Cart />} />} />
+  );
   return (
     <BrowserRouter>
       <Routes>
