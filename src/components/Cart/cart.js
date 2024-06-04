1761f7bc499b62710642700dfd8cb95726d0850c
sharukhan <sharukhan@webkorps.com>
2024-06-04 21:01:26 +0530

    cart page cart item views and summary section

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import CustomNavbar from "../Navbar/navbar";
 import GlobalComponents from "../_Global/index";
 import CartItemsUtil from "../../utils/cartUtils";
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const states = GlobalComponents.states;
 
@@ -27,8 +27,101 @@ const Cart = () => {
     fetchItems();
   }, []);
 
+  const renderCartItemColumnHeaders = () => {
+    return (
+      <Row>
+        <Col>Item</Col>
+        <Col>Price</Col>
+        <Col>Quantity</Col>
+        <Col>Sub Total</Col>
+        <Col xs={1}></Col>
+      </Row>
+    );
+  };
+
+  const renderCartItem = (product) => {
+    return (
+      <Row>
+        <Col>{product.title}</Col>
+        <Col>${product.price}</Col>
+        <Col>{product.quantity}</Col>
+        <Col>${product.quantity * product.price}</Col>
+        <Col xs={1} className="badge bg-danger">
+          DEL
+        </Col>
+      </Row>
+    );
+  };
+
+  const renderCartItems = () => {
+    return (
+      <Col md={8} className="mb-3 p-3">
+        <h4>Cart Items</h4>
+        <Row className="flex-column p-2">
+          <Col className="border-danger border-bottom border-3 mb-2">
+            {renderCartItemColumnHeaders()}
+          </Col>
+          {cartItems.map((product) => (
+            <Col className="border-bottom mb-2" key={product.id}>
+              {renderCartItem(product)}
+            </Col>
+          ))}
+        </Row>
+      </Col>
+    );
+  };
+
+  const renderTotalPrice = () => {
+    const summaryDetails = {
+      subtotal: cartItems.reduce((a, b) => {
+        return a.price * a.quantity + b.price * b.quantity;
+      }),
+      deliveryCharges: 100,
+      discount: 50,
+    };
+    const orderTotal =
+      summaryDetails.subtotal +
+      summaryDetails.deliveryCharges -
+      summaryDetails.discount;
+    return (
+      <Col md={4} className="border p-3">
+        <h3 className="border-bottom border-danger border-3">Summary</h3>
+        <Row>
+          <Col>Sub Total</Col>
+          <Col className="text-end">${summaryDetails.subtotal}</Col>
+        </Row>
+        <Row>
+          <Col>Delivery Charges</Col>
+          <Col className="text-end">${summaryDetails.deliveryCharges}</Col>
+        </Row>
+        <Row>
+          <Col>Discount</Col>
+          <Col className="text-end">-${summaryDetails.discount}</Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>Order Total</Col>
+          <Col className="text-end">${orderTotal}</Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button className="w-100 mb-3 mt-2">Proceed to Checkout</Button>
+          </Col>
+        </Row>
+      </Col>
+    );
+  };
+
   const renderCartsComponent = () => {
-    return cartItems.map((item) => <h1>{item.title}</h1>);
+    return (
+      <Container>
+        <Row>
+          {renderCartItems()}
+          {renderTotalPrice()}
+        </Row>
+      </Container>
+    );
   };
 
   return (
